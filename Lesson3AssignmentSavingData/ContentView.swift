ae166881147df7146b04533c0bd5e9d4c3070594
Sandy Gomez <sgomez1029@gmail.com>
2024-04-19 13:51:57 -0700

    Initial Commit

---
+++ Lesson3AssignmentSavingData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lesson3AssignmentSavingData
+//
+//  Created by Sandra Gomez on 4/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lesson3AssignmentSavingData/Lesson3AssignmentSavingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson3AssignmentSavingDataApp.swift
+//  Lesson3AssignmentSavingData
+//
+//  Created by Sandra Gomez on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson3AssignmentSavingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
